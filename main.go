ff49aafceb3ef66e7787e9c6167acc4125988a44
Aleksandar Dimov <isavitaisa@gmail.com>
2022-12-02 07:36:55 +0100

    day2

--- main.go
+++ main.go
@@ -11,6 +11,88 @@ import (
 
 func main() {
 	fmt.Printf("day1\n answer1: %d\n answer2: %d", day1Task1(), day1Task2())
+	fmt.Printf("\nday2\n answer1: %d\n answer2: %d", day2Task1(), day2Task2())
+}
+
+func day2Task2() int {
+	input := readInput("assets/input2.txt")
+	score := 0
+	for _, line := range strings.Split(input, "\n") {
+		// Opponent: A for Rock, B for Paper, and C for Scissors
+		// Player outcome: X - lose, Y - draw, and Z - win
+		strategy := strings.Split(line, " ")
+		outcome := strategy[1]
+		opponent := strategy[0]
+		score += day2CalcExpectScore(outcome, opponent)
+	}
+
+	return score
+}
+
+func day2CalcExpectScore(outcome, opponent string) int {
+	var player string
+	if outcome == "Z" { // Player wins
+		if opponent == "A" {
+			player = "B"
+		} else if opponent == "B" {
+			player = "C"
+		} else {
+			player = "A"
+		}
+	} else if outcome == "X" { // Player loses
+		if opponent == "A" {
+			player = "C"
+		} else if opponent == "B" {
+			player = "A"
+		} else {
+			player = "B"
+		}
+	} else { // Draw
+		player = opponent
+	}
+	return day2RoundScore(player, opponent) + day2PlayerScore(player)
+}
+
+func day2Task1() int {
+	input := readInput("assets/input2.txt")
+	score := 0
+	for _, line := range strings.Split(input, "\n") {
+		// Opponent: A for Rock, B for Paper, and C for Scissors
+		// Player: X for Rock, Y for Paper, and Z for Scissors
+		strategy := strings.Split(line, " ")
+		var player string
+		if strategy[1] == "X" {
+			player = "A"
+		} else if strategy[1] == "Y" {
+			player = "B"
+		} else {
+			player = "C"
+		}
+		opponent := strategy[0]
+		score += day2RoundScore(player, opponent) + day2PlayerScore(player)
+	}
+
+	return score
+}
+
+func day2PlayerScore(player string) int {
+	if player == "C" {
+		return 3
+	} else if player == "B" {
+		return 2
+	} else {
+		return 1
+	}
+}
+
+func day2RoundScore(p1, p2 string) int {
+	if (p1 == "A" && p2 == "C") || (p1 == "B" && p2 == "A") || (p1 == "C" && p2 == "B") {
+		return 6
+	} else if (p1 == "C" && p2 == "A") || (p1 == "A" && p2 == "B") || (p1 == "B" && p2 == "C") {
+		return 0
+	} else {
+		return 3
+	}
 }
 
 func day1Task2() int64 {
